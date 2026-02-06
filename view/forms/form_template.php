0286c7a0efc6e97aabdae5d818729fabd21a947e
Guillermo López Acosta <memo1145222@gmail.com>
2026-02-05 21:23:32 -0700

    correcciones vista

--- view/forms/form_template.php
+++ view/forms/form_template.php
@@ -1,19 +1,19 @@
 
            <div>
-               <label for="name" id="name">Nombre:</label>
+               <label for="name" class="form-label">Nombre:</label>
                <input type="text" name="name" class="form-control" id="name">
            </div>
            <div>
-               <label for="first_surname" id="name">Apellido:</label>
-               <input type="first_surname" name="first_surname" class="form-control"  id="first_surname">
+               <label for="first_surname" class="form-label">Apellido:</label>
+               <input type="first_surname" name="first_surname" class="form-control" id="first_surname">
            </div>
 
            <div>
-               <label for="curp" id="name">CURP:</label>
-               <input type="text" name="curp" class="form-control"  id="curp">
+               <label for="curp" class="form-label">CURP:</label>
+               <input type="text" name="curp" class="form-control" id="curp">
            </div>
 
            <div>
-               <label for="rfc" id="name">RFC:</label>
+               <label for="rfc" class="form-label">RFC:</label>
                <input type="text" name="rfc" class="form-control"  id="rfc">
            </div>
